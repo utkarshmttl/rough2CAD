90b2acdfe6f10c77e5860f54fa7498acb743610c
Aryan Arora <aryanarora278@gmail.com>
2018-02-13 10:11:15 +0530

    code commented

--- corner detection/corner detection.py
+++ corner detection/corner detection.py
@@ -1,15 +1,18 @@
 import cv2
 import numpy as np
-
+## pixel values takes bgr as params, not rgb.
 img = cv2.imread('corners.jpg')
-
+## cvtColor = convert color
 grayed = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
+##float32 is used for precision.
 grayed = np.float32(grayed)
 
+## goodFeaturesToTrack(imgSource, maxNumberOfCorners, Quality, MinimumDistBetweenCorners)
 corners = cv2.goodFeaturesToTrack(grayed,100,0.01,10)
 corners = np.int0(corners)
 
+##marking corners
 for corner in corners:
     x,y = corner.ravel()
     cv2.circle(img,(x,y),3,255,-1000)
